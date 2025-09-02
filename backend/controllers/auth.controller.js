592efb1f62643b5ecd39ce5b5f6ecacff278a8f0
NobiSamid <nobisamid@gmail.com>
2025-09-02 18:22:36 +0600

    server set

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (req, res) => {
+  res.send('Signup controller');
+}
+
+export const login = async (req, res) => {
+  res.send('login controller');
+}
+
+export const logout = async (req, res) => {
+  res.send('logout controller');
+}
---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+import authRoute from './routes/auth.route.js';
+
+dotenv.config();
+const app = express();
+
+console.log(process.env.PORT);
+const PORT = process.env.PORT || 5000;
+
+app.use('/api/auth', authRoute);
+
+app.get("/", (req, res) => {
+  res.send("Server is working fine");
+});
+
+app.listen(PORT, () => {
+  console.log('Server is running on http://localhost:' + PORT);
+});
