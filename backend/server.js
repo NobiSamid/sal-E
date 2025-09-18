6993f48deba14c8221d6c83b1fec12a97fcf65a9
NobiSamid <nobisamid@gmail.com>
2025-09-18 16:12:41 -0700

    coupon route added

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 import authRoute from './routes/auth.route.js';
 import productRoute from './routes/product.route.js';
 import cartRoute from './routes/cart.route.js';
+import couponRoute from './routes/coupon.route.js';
 
 
 import { connectDB } from './lib/db.js';
@@ -20,6 +21,7 @@ app.use(cookieParser());
 app.use('/api/auth', authRoute);
 app.use('/api/products', productRoute);
 app.use('/api/cart', cartRoute);
+app.use('/api/coupons', couponRoute);
 
 
 app.get("/", (req, res) => {
