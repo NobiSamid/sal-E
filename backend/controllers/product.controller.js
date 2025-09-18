41add08aa5199ec4587b9dbd33b67925e84932a6
NobiSamid <nobisamid@gmail.com>
2025-09-18 10:45:39 -0700

    product route done

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,5 +1,8 @@
 import { client } from '../lib/redis.js';
+import cloudinary from '../lib/cloudinary.js';
 import product from '../models/product.model.js';
+import Product from '../models/product.model.js';
+import e from 'express';
 
 
 export const getAllProducts = async (req, res) => {
@@ -36,3 +39,111 @@ export const getFeaturedProducts = async (req, res) => {
     res.status(500).json({ message: 'Server error hoise', error: error.message });
   }
 }
+
+export const createProduct = async (req, res) => {
+  try {
+    const { name, description, price, image, category } = req.body;
+    
+    let cloudinaryResponse = null;
+
+    if(image){
+      cloudinaryResponse = await cloudinary.uploader.upload(image, {folder: 'products'})
+    }
+
+    const product = await product.create({
+      name,
+      description,
+      price,
+      image: cloudinaryResponse?.secure_url ? cloudinaryResponse.secure_url:"",
+      category
+    })
+    res.status(201).json({ message: 'Product created successfully', product });
+  }catch (error) {
+    console.log("Error in createProduct:", error.message);
+    res.status(500).json({ message: 'Server error hoise', error: error.message });
+  }
+};
+
+export const deleteProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    if (product.image) {
+      const publicId = product.image.split('/').pop().split('.')[0]; // Extract public ID from URL
+      try{
+        await cloudinary.uploader.destroy(`products/${publicId}`);
+        console.log("Image deleted from Cloudinary");
+      }catch(error){
+        console.log("Error deleting image from Cloudinary:", error.message);
+      }
+    }
+
+    await Product.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: 'Product deleted successfully'})
+    
+  } catch (error) {
+    console.log("Error in deleteProduct:", error.message);
+    res.status(500).json({ message: 'Server error hoise', error: error.message });
+  }  
+
+}
+
+export const getRecommendedProducts = async (req, res) => {
+  try {
+    const products = await Product.aggregate([
+      {
+        $sample: {size:3}
+      },
+      {
+        $project:{
+          _id:1,
+          name:1,
+          description:1,
+          price:1,
+          image:1
+        }
+      }
+    ])
+    res.status(200).json(products);
+  } catch (error) {
+    console.log("Error in getRecommendedProducts:", error.message);
+    res.status(500).json({ message: 'Server error hoise', error: error.message });
+  }
+};
+
+export const getProductsBYCategory = async (req, res) => {
+  const {category} = req.params;
+  try {
+    const products = await Product.find({category});
+    res.status(200).json(products);
+  } catch (error) {
+    console.log("Error in getProductsBYCategory:", error.message);
+    res.status(500).json({ message: 'Server error hoise', error: error.message });
+  }
+};
+
+export const toggleFeaturedProduct = async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if(product){
+    product.isFeatured = !product.isFeatured;
+    const updatedProduct = await product.save();
+    await updateFeaturedProductsCache();
+    res.status(200).json({message: 'Product updated successfully', product: updatedProduct});
+  } else{
+    res.status(404).json({message: 'Product not found'});
+  }
+}
+
+async function updateFeaturedProductsCache(){
+  try{
+    // the len() method is used to return plain js objects instead of full mongoose docs. this can significantly improve performance when we only need to read the data
+
+    const featuredProducts = await Product.find({isFeatured: true}).lean();
+    await client.set("featured_products", JSON.stringify(featuredProducts));
+  } catch (error){
+    console.log("Error updating featured products cache:", error.message);
+  }
+}  
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,12 +1,15 @@
 import express from 'express';
-import { getAllProducts } from '../controllers/product.controller.js';
+import { createProduct , deleteProduct,getAllProducts, getFeaturedProducts, getProductsBYCategory, getRecommendedProducts, toggleFeaturedProduct } from '../controllers/product.controller.js';
 import { adminRoute, protectRoute } from '../middleware/auth.middleware.js';
-import { getFeaturedProducts } from '../controllers/product.controller.js';
-
 
 const router = express.Router();
 
 router.get('/', protectRoute, adminRoute, getAllProducts);
 router.get('/featured', getFeaturedProducts);
+router.get('/category/:category', getProductsBYCategory);
+router.get('/recommendations', getRecommendedProducts);
+router.post('/', protectRoute, adminRoute, createProduct);
+router.patch('/:id', protectRoute, adminRoute, toggleFeaturedProduct);
+router.post('/:id', protectRoute, adminRoute, deleteProduct);
 
 export default router;
