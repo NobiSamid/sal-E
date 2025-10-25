2f51de1b524e087e3f870cf79e17e39f753ab09a
NobiSamid <nobisamid@gmail.com>
2025-10-25 23:00:33 +0600

    http:// removed from comment

--- backend/server.js
+++ backend/server.js
@@ -45,6 +45,6 @@ app.get("/", (req, res) => {
 
 connectDB().then(() => {
     app.listen(PORT, () => {
-      console.log('Server is running on http://localhost:' + PORT);
+      console.log('Server is running on localhost:' + PORT);
     });
 })
